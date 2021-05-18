1d9dd2441843ee05c3a23c161304e9ab592fa435
cevherertan <69977907+cevherertan@users.noreply.github.com>
2021-05-18 13:26:52 +0300

    Add project files.

---
+++ WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void listele()
+        {
+            baglanti.ac();
+            SqlCommand komut = new SqlCommand("select ogrenci.ONo,ogrenci.OAdi,Ogrenci.OSoyadi,Bolum.BAdi,Ders.DAdi,Puan.Vize*0.4+Puan.Final*0.6 as Ortalama From Ogrenci inner join Puan on Puan.Ono=Ogrenci.Ono inner join Ders on Ders.Dno=Puan.Dno inner join Bolum on Bolum.Bno=Ders.Bno Where Ogrenci.oAdi like @a or Ogrenci.OSoyadi like @a or Bolum.Badi like @a or Ders.Dadi like @a or Ogrenci.ONo like @a",baglanti.baglan);
+            komut.Parameters.AddWithValue("@a","%"+textBox1.Text+"%");
+            DataTable liste = new DataTable();
+            liste.Load(komut.ExecuteReader());
+            dataGridView1.DataSource = liste;
+            baglanti.kapat();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            listele();           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listele();
+        }
+    }
+}
---
+++ WindowsFormsApplication5/baglanti.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication5
+{
+    class baglanti
+    {
+        public static SqlConnection baglan = new SqlConnection(@"Data Source=DESKTOP-0HLJ5NJ\SQLEXPRESS;Initial Catalog=ogr_isleri;Integrated Security=True");
+        public static void ac()
+        {
+            baglan.Open();
+        }
+        public static void kapat()
+        {
+            baglan.Close();
+        }
+    }
+}
